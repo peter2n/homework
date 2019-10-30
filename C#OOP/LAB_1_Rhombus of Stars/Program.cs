7ec1d2558e3614b6f55ce1cd9487a433e9bb0c51
peter2n <peter2n@abv.bg>
2019-10-30 16:00:31 +0200

    Labs Abstract

---
+++ C#OOP/LAB_1_Rhombus of Stars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+namespace LAB_1_Rhombus_of_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxStars = int.Parse(Console.ReadLine());
+            int rowLength = 2 * maxStars - 1;
+
+            for (int i = 1; i <= maxStars; i++)
+            {
+                PrintRow(maxStars, i);
+            }
+            for (int i = maxStars - 1; i > 0; i--)
+            {
+                PrintRow(maxStars, i);
+            }
+
+        }
+
+        static void PrintRow(int maxSt, int starCount)
+        {
+            for (int i = 0; i < maxSt - starCount; i++)
+                Console.Write(" ");
+            for (int col = 1; col < starCount; col++)
+                Console.Write("* ");
+            Console.WriteLine("*");
+        }
+
+    }
+}
+
---
+++ C#OOP/LAB_2_Point in Rectangle/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace LAB_2_Point_in_Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //"{topLeftX} {topLeftY} {bottomRightX} {bottomRightY}".
+
+            int[] coord = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            MyRectangle rect = new MyRectangle(coord[0], coord[1], coord[2], coord[3]);
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] aPointCoord = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+                Point myPoint = new Point(aPointCoord[0], aPointCoord[1]);
+
+                rect.Contains(myPoint);
+
+            }
+
+
+
+        }
+
+        class Point
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            Point(int x, int y)
+            {
+                this.x = x;
+                this.y =
+            }
+        }
+        class MyRectangle
+        {
+            public Point TopLeft { get; set; }
+            public Point LowerRight { get; set; }
+
+            public MyRectangle(int topLX, int topLY, int lowRX, int lowRY)
+            {
+                Point TopLeft = new Point();
+                Point LowerRight = new Point();
+
+                TopLeft.x = topLX;
+                TopLeft.y = topLY;
+                LowerRight.x = lowRX;
+                LowerRight.y = lowRY;
+
+
+            }
+
+            public bool Contains(Point aPoint)
+            {
+                if (aPoint.x >= TopLeft.x && aPoint.x <= LowerRight.x && aPoint.y >= LowerRight.y && aPoint.y <= TopLeft.y)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+
+
+    }
+}
