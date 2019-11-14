a12719f7ac6d23fa2a5afd1b3f83d704677e09f8
peter2n <peter2n@abv.bg>
2019-11-14 15:28:25 +0200

    encapsulation

---
+++ C#OOP/03_Encapsulation/LAB_1_Sort Persons by Name and Age/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    class Person
+    {
+        public Person(string firstName, string lastName, int age,decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+
+        }
+
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            //return $"{this.FirstName } {this.LastName } is {this.Age } years old.";
+            return $"{this.FirstName } {this.LastName } receives {Salary:f2} leva.";
+        }
+
+        internal void IncreaseSalary(decimal persentage)
+        {
+            if (this.Age<30)
+            {
+                persentage /= 2;
+            }
+
+            this.Salary *= (100 + persentage) / 100;
+        }
+    }
+}
---
+++ C#OOP/03_Encapsulation/LAB_3_Validation/Person.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        public string FirstName
+        {
+            get => this.firstName;
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException(nameof(firstName), "First name must be longer then 2 chars");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => this.lastName;
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException(nameof(lastName), "First name must be longer then 2 chars");
+                }
+                this.LastName = value;
+            }
+
+
+        }
+        public int Age
+        {
+            get => this.age;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(age), "Age must be greater than zero");
+                }
+                this.Age = value;
+            }
+        }
+
+
+        public decimal Salary { 
+            get => this.salary;
+            private set
+            {
+                if (value<460)
+                {
+                    throw new ArgumentException(nameof(salary), "Salary must be at least 460 lv");
+                }
+                this.Salary = value;
+
+            }
+        }
+
+        public override string ToString()
+        {
+            //return $"{this.FirstName } {this.LastName } is {this.Age } years old.";
+            return $"{this.FirstName } {this.LastName } receives {Salary:f2} leva.";
+        }
+
+        internal void IncreaseSalary(decimal persentage)
+        {
+            if (this.Age < 30)
+            {
+                persentage /= 2;
+            }
+
+            this.Salary *= (100 + persentage) / 100;
+        }
+    }
+}
